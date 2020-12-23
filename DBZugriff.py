13614d21f500c5160ca0dbcc536c738f8410a9f9
Ikarus2105 <73403214+Ikarus2105@users.noreply.github.com>
2020-12-23 11:31:39 +0100

    v2
    v2

--- DBZugriff.py
+++ DBZugriff.py
@@ -1,27 +1,30 @@
 from FileRedirection import FileRedirection
 import json
+import os
 
 class DBZugriff:
     def __init__(self):
         self.json_Datei = "FileRedirection.json"
-        with open(self.json_Datei, 'w') as f:
-            f.write('{ "FileRedirections": []  }')
+        if(os.stat(self.json_Datei).st_size == 0):
+            with open(self.json_Datei, 'w') as f:
+                f.write('{ "FileRedirections": []  }') 
 
     def __getJSONData(self):
         with open(self.json_Datei, 'r') as f:
             jsonFRData = json.load(f)
             return jsonFRData
     def __setJSONData(self, pData):
+        print(pData)
         with open(self.json_Datei, 'w') as f:
             json.dump(pData, f, indent=4)
 
     def writeDB(self, pFileRedirection : FileRedirection):
         self.file_redirection = pFileRedirection
         dictFileRedirection = {
-            "Name": self.file_redirection.name,
-            "Source": self.file_redirection.path_source_folder,
-            "Target": self.file_redirection.path_target_folder,
-            "Datatype": self.file_redirection.data_type
+            "name": self.file_redirection.name,
+            "source": self.file_redirection.path_source_folder,
+            "target": self.file_redirection.path_target_folder,
+            "datatype": self.file_redirection.data_type
         }
         jsonFileRedirection = json.dumps(dictFileRedirection)
         jsonFRList = self.__getJSONData()
